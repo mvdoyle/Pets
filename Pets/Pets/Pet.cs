e383a582128cbf3d45b64155d0d4f9dfbef0011d
mvdoyle <mdoyle@truecoders.io>
2019-09-18 09:04:28 -0500

    added the pet class

---
+++ Pets/Pets/Pet.cs
@@ -0,0 +1,15 @@
+﻿using System;
+namespace Pets
+{
+    public class Pet
+    {
+        public int Age { get; set; }
+        public string Name { get; set; }
+        public int Legs { get; set; }
+        public string Breed { get; set; }
+
+        public Pet()
+        {
+        }
+    }
+}
